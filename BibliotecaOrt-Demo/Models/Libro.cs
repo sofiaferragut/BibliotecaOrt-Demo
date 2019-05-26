774d034a22976847108b91ecbf525cabf1984594
Sofia.Ferragut <sofia.ferragut@gmail.com>
2019-05-26 15:36:05 -0300

    Commit inicial

---
+++ BibliotecaOrt-Demo/Models/Libro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaOrt_Demo.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public Libro LibroHardcodeado() {
+            Libro l = new Libro();
+            l.Nombre = "HarryPotter";
+            return l;
+        }
+        
+    }
+}
